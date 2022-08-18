93dd24159173225fa65f60721c715b08e3f754b3
Japneet Singh <singhjapneetssa@gmail.com>
2022-08-19 02:25:18 +0530

    added execvp support, child fork

--- shell.c
+++ shell.c
@@ -2,22 +2,66 @@
 #include<unistd.h>
 #include<signal.h>
 #include<stdlib.h>
+#include<string.h>
 
 #define PATH_BUFFER_SIZE 256
 #define INPUT_BUFFER_SIZE 1024
+#define BUILT_IN_COMMANDS_SIZE = 2;
 
 void signalHandler(int signal){
     exit(0);
 }
 
+int isPsHistory(char* input){
+    if(strcmp(input,"ps_history")==0){
+        return 1;
+    }
+    return 0;
+}
+
+void split(char* input, char* args[512]){
+    int i = 0;
+    char * token = strtok(input, " ");
+    while( token != NULL ) {
+        args[i]=token;
+        i++;
+        token = strtok(NULL, " ");
+    }
+    args[i]=NULL;
+}
+
+int isCmdHistory(char* input){
+    if(strcmp(input,"cmd_history")==0){
+        return 1;
+    }
+    return 0;
+}
+
 int main(){
     char currentDirectory[PATH_BUFFER_SIZE];
     char input[INPUT_BUFFER_SIZE];
+    char *args[512];
     signal(SIGINT, signalHandler);
     while(1){
         getcwd(currentDirectory,  PATH_BUFFER_SIZE);
         printf("%s~$ ",currentDirectory);
         scanf("%[^\n]%*c",input); // scanf with regex to take spaces as input and not exit
-        printf("%s\n",input);
+        split(input, args);
+        if(isPsHistory(args[0])){
+            printf("Printing Ps history");
+        }else if(isCmdHistory(args[0])){
+            printf("Printing cmd history");
+        }else{
+            int childProcessID = fork();
+            if(childProcessID == 0){
+                // this is the child process 
+                // execute the command here
+                execvp(args[0], args);
+            }else{
+                wait(NULL);
+                printf("Finished with child process %d \n", childProcessID);
+            }
+        }
     }
+    return 0;
 }
